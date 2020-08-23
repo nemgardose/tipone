d3d03a8a6e335d0d14bb5013499fc4954e1879cc
nemgardose <gardosenemwel@gmail.com>
2020-08-23 12:06:39 +0800

    Added model and migrations for Company and Employee's table

---
+++ database/migrations/2020_08_23_023215_create_employees_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmployeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('company_id');
+            $table->string('first_name')->nullable(false);
+            $table->string('last_name')->nullable(false);
+            $table->string('email')->unique();
+            $table->string('phone');
+            $table->timestamps();
+
+            $table->foreign('company_id')->references('id')->on('companies');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+}
